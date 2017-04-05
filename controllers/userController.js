1872a2282bb6ce249a5544840c0e1bbee5351b3a
Jesus <jesuspereyrad@gmail.com>
2017-04-04 23:32:32 -0300

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,13 +19,12 @@ module.exports = function(app) {
 
 	loginUser = function(req, res) {
 		console.log(req.body);
-		console.log(req.params.email, req.params.password);
-		user.findOne({email: req.params.email}), function(err, current) {
+		user.findOne({email: req.body.email}), function(err, current) {
 			if(err) {
 				res.status(404).send("User not found");
 			}
 			else {
-				if (current.password == req.params.password) {
+				if (current.password == req.body.password) {
 					res.status(200).json(current);
 				}
 				else
